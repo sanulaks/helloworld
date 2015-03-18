6b6c0b8d1d9f3f8ddc4b986c687a3e4e6f0fb5b4
santhanalakshmi <santhanalakshmi@ebmdell7-PC.ebmnadi.local>
2015-03-18 14:51:17 +0530

    Initial checking of hello.

---
+++ helloworld/src/myapp/helloworld.java
@@ -0,0 +1,19 @@
+package myapp;
+
+public class helloworld {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+SalesData sale = new SalesData();
+displayGreeting();
+
+	}
+
+	private static void displayGreeting() {
+		// TODO Auto-generated method stub
+	System.out.println("welcome");
+	System.out.println("hai");
+	}
+
+	
+}
