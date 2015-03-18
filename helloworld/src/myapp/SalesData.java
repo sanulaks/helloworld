1295f732051bfc6b47608f6f9baa4f9c6b088d8f
santhanalakshmi <santhanalakshmi@ebmdell7-PC.ebmnadi.local>
2015-03-18 15:32:57 +0530

    added ignore

--- helloworld/src/myapp/SalesData.java
+++ helloworld/src/myapp/SalesData.java
@@ -1,5 +1,14 @@
 package myapp;
 
 public class SalesData {
+	public static void main (String args[])
+	{
+	
 
+	}
+
+public void dispaly() {
+		// TODO Auto-generated method stub
+		System.out.println("hai");
+	}
 }
